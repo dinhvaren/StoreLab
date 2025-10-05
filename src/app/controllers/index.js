08018b954401d1ec79fea58cc9e8d309b1a5abfc
dinhvaren <hqhuyzzzz@gmail.com>
2025-10-05 19:02:32 +0700

    update AdminController

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -2,10 +2,12 @@ const HomeController = require("./HomeController");
 const AuthController = require("./AuthController");
 const OdersController = require("./OdersController");
 const UsersController = require("./UsersController");
+const AdminController = require("./AdminController");
 
 module.exports = {
     HomeController,
     AuthController,
     OdersController,
-    UsersController
+    UsersController,
+    AdminController
 }
